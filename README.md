7bdcc3b1ed5d2044dd45835f65d116dc1408a3b2
f <beteljesed@gmail.com>
2026-02-12 21:48:06 +0300

    sensors added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rtos-basic-project
+
+
+
+Görev Dağılımı (Tasks): İşleri nasıl böleriz?
+
+Haberleşme (Queues): Görevler birbirine nasıl veri gönderir?
+
+Kaynak Paylaşımı (Semaphores/Mutex): Ortak kullanılan bir cihazı (LCD gibi) nasıl yönetiriz?
+
+Hata Yönetimi (Deadlock/Priority Inversion): İşler ne zaman ters gider?
--- main.cpp
+++ main.cpp
@@ -1,39 +1,42 @@
-
+#include <Wire.h>
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 
 // --- Pin Tanımlamaları ---
 #define DHTPIN 15
 #define DHTTYPE DHT22
-#define POT_PIN 34
-#define SERVO_PIN 13
 
+// GLOBAL TANIMLAMALAR
 QueueHandle_t sicaklikKutusu;
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+DHT dht(DHTPIN, DHTTYPE);
+
 
 
 // Task_Sensor seri porta her 1 saniyede bir sicaklık 24 derece yazacak
 void Task_Sensor(void *pvParameters) {
-  int dummySicaklik = 24;
+  
   while(1) { 
-    
-    Serial.println("Sicaklik " + %d + " derece...", dummySicaklik);
-    xQueueSend(sicaklikKutusu, &dummySicaklik, 0);
+    float sicaklik_degeri = dht.readTemperature();  
+    xQueueSend(sicaklikKutusu, &sicaklik_degeri, 0);
     // İşlemciyi yormamak için bu görevi uyutuyoruz (1 saniye)
     vTaskDelay(1000 / portTICK_PERIOD_MS); 
   }
 }
 
 
-// Task_LCD seri porta her 3 saniyede bir LCD ekran güncelleniyor... yazacak
 void Task_LCD(void *pvParameters){
-  int gelenVeri;
+  float gelenVeri;
   while(1){
     
 
     if(xQueueReceive(sicaklikKutusu, &gelenVeri, portMAX_DELAY)){
-      Serial.println("LCD ekran guncelleniyor...");
-      Serial.print("Kuyruktan yeni veri geldi");
-      Serial.println(gelenVeri);
+        lcd.clear();
+        lcd.setCursor(0, 0);       // Üst satırın başlangıcı
+        lcd.print("Sicaklik:");
+        lcd.setCursor(0, 1);       // Alt satırın başlangıcı
+        lcd.print(gelenVeri);
+        lcd.print(" C");
     }
   }
 }
@@ -44,11 +47,17 @@ void Task_LCD(void *pvParameters){
 
 void setup() {
   Serial.begin(115200);
+  
+  // Ekranı başlatalım
+  lcd.init(); 
+  lcd.backlight();
+  
+  dht.begin(); // dht sensörünü de başlattık
   Serial.println("Sistem baslatiliyor...");
 
 
   // int türünde 10 adet kutu
-  sicaklikKutusu = xQueueCreate(10, sizeof(int));
+  sicaklikKutusu = xQueueCreate(10, sizeof(float));
 
 
   // --- 2. GÖREV OLUŞTURMA (xTaskCreate) ---
