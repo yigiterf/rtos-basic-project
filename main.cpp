6895f3853126a4f7fabdd6b48fceb995ae5b8eb4
f <beteljesed@gmail.com>
2026-02-13 23:14:39 +0300

    dht sensor

--- main.cpp
+++ main.cpp
@@ -1,26 +1,39 @@
 #include <Wire.h>
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
+#include <ESP32Servo.h>
 
 // --- Pin Tanımlamaları ---
-#define DHTPIN 15
+#define DHTPIN 4
 #define DHTTYPE DHT22
 
 // GLOBAL TANIMLAMALAR
 QueueHandle_t sicaklikKutusu;
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 DHT dht(DHTPIN, DHTTYPE);
+SemaphoreHandle_t lcdAnahtari;
+Servo pencereServosu;
 
 
-
-// Task_Sensor seri porta her 1 saniyede bir sicaklık 24 derece yazacak
 void Task_Sensor(void *pvParameters) {
-  
+  // Sensörün kararlı hale gelmesi için döngü öncesi biraz bekle
+  vTaskDelay(2000 / portTICK_PERIOD_MS);
+
   while(1) { 
+    // Okuma yapmadan önce sensöre kısa bir süre tanı
     float sicaklik_degeri = dht.readTemperature();  
-    xQueueSend(sicaklikKutusu, &sicaklik_degeri, 0);
-    // İşlemciyi yormamak için bu görevi uyutuyoruz (1 saniye)
-    vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    
+    // Eğer okuma başarısızsa (nan ise) kuyruğa gönderme, bir sonraki döngüyü bekle
+    if (!isnan(sicaklik_degeri)) {
+      xQueueSend(sicaklikKutusu, &sicaklik_degeri, 0);
+      Serial.print("Okunan sicaklik: ");
+      Serial.println(sicaklik_degeri);
+    } else {
+      Serial.println("Sensör verisi okunamadı!");
+    }
+    
+    // Gecikmeyi 2 saniyeye çıkar (DHT22 için en sağlıklı süre budur)
+    vTaskDelay(2000 / portTICK_PERIOD_MS); 
   }
 }
 
@@ -28,29 +41,42 @@ void Task_Sensor(void *pvParameters) {
 void Task_LCD(void *pvParameters){
   float gelenVeri;
   while(1){
-    
-
-    if(xQueueReceive(sicaklikKutusu, &gelenVeri, portMAX_DELAY)){
-        lcd.clear();
-        lcd.setCursor(0, 0);       // Üst satırın başlangıcı
-        lcd.print("Sicaklik:");
-        lcd.setCursor(0, 1);       // Alt satırın başlangıcı
-        lcd.print(gelenVeri);
-        lcd.print(" C");
-    }
+      if(xQueueReceive(sicaklikKutusu, &gelenVeri, portMAX_DELAY)){
+          if (xSemaphoreTake(lcdAnahtari, portMAX_DELAY)) {
+              // 1. Sıcaklığı yazdır
+              lcd.setCursor(0, 0); lcd.print("Isi: "); 
+              lcd.print(gelenVeri);
+              
+              // 2. Kontrol et ve gerekirse alarm bas
+              lcd.setCursor(0, 1);
+              if(gelenVeri > 30.0){
+                lcd.print("!!! TEHLIKE !!!");
+                pencereServosu.write(90); // pencereyi aç
+              }
+              else{
+                pencereServosu.write(0); // pencereyi kapat
+                lcd.print("                "); // Temizle
+              }
+              xSemaphoreGive(lcdAnahtari);
+          }
+      }
   }
 }
 
 
 
 
-
 void setup() {
   Serial.begin(115200);
+
+  float dummyVal = 25.0;
   
   // Ekranı başlatalım
   lcd.init(); 
   lcd.backlight();
+  lcdAnahtari = xSemaphoreCreateMutex();
+  pencereServosu.attach(13);
+
   
   dht.begin(); // dht sensörünü de başlattık
   Serial.println("Sistem baslatiliyor...");
@@ -80,7 +106,7 @@ void setup() {
   );
 
 
-
+  xQueueSend(sicaklikKutusu, &dummyVal, 0);
 
 }
 
@@ -88,3 +114,13 @@ void loop() {
   // RTOS kullanırken burası boş kalır. 
   // Çünkü artık kontrol setup() içinde oluşturduğumuz görevlere geçti.
 }
+
+
+/*
+xQueueReceive(sicaklikKutusu, &gelenVeri, portMAX_DELAY)
+  sicaklikKutusu adındaki sıraya bakar
+  Eğer içinde veri varsa o veriyi çeker ve gelenVeri içine kopyalar
+  portMAX_DELAY ise : git queue sırasına bak, eğer veri yoksa veri gelene kadar uyu ve bekle demektir
+
+
+*/
